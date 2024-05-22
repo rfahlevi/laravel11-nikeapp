6931b2650c5ffa2de0d6f51c317c372abd4d7f30
Reza Fahlevi <rezafahlevi55@gmail.com>
2024-05-22 08:08:22 +0700

    Create Fetch Product Categories API

---
+++ app/Http/Controllers/Api/ProductCategoryController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Models\ProductCategory;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductCategoryResource;
+
+class ProductCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = ProductCategory::all();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Categories retrieved successfully.',
+            'data' => ProductCategoryResource::collection($categories),
+        ], 200);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\ProductCategoryController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
@@ -17,6 +18,8 @@ Route::middleware('auth:sanctum')->group(function() {
 
     Route::get('/products', [ProductController::class, 'index']);
     Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+
+    Route::get('/product-categories', [ProductCategoryController::class, 'index']);
 });
 
 
