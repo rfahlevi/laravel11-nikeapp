95daf17561dfe3d97e0aa46afb6fcfacded146cc
Reza Fahlevi <rezafahlevi55@gmail.com>
2024-05-26 21:33:25 +0700

    fix login blade

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -27,6 +27,7 @@
                 </div>
 
                 <div class="form-group">
+                    <label for="password">Password</label>
                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
                         name="password" tabindex="2">
                     @error('password')
